33edc960b964c8eede7fa220017b407f88ce8fd3
Muji <gerelt-od.munkhjin.jh@tut.jp>
2026-02-05 19:30:07 +0900

    Update test_cases.py

--- test_cases.py
+++ test_cases.py
@@ -14,6 +14,9 @@ class TestCalc(unittest.TestCase):
 
     def test_sample4(self):
         self.assertEqual(-1, calc(0.1, 999))
+        
+    def test_sample5(self):
+        self.assertEqual(-1, calc(0, 999))
 
 
                 
